d6122cef9e5d11349909662523a926fa94d54697
Agustín Acosta <agustin1acosta090@gmail.com>
2024-10-10 23:31:52 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-	<footer>
-		<h4>Realizado por Agustín Acosta.</h4>
-		<h4>Actividad Integradora Obligatoria Módulo 2</h4>
-			<p>
-				Las imágenes fueron sacadas de Google.
-			</p>
-	</footer>
-</body>
-</html>
